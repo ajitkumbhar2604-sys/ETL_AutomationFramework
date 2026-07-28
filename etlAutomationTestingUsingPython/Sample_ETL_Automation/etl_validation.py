53f6ca575faa59e7829e9465fb055d743e8ac5b8
ajitkumbhar2604-sys <ajitkumbhar2604@gmail.com>
2026-07-28 16:54:05 +0530

    Check_null, dataCompare, outOfRange validation added on 28/07/2026 at 4.53pm

--- etlAutomationTestingUsingPython/Sample_ETL_Automation/etl_validation.py
+++ etlAutomationTestingUsingPython/Sample_ETL_Automation/etl_validation.py
@@ -1,5 +1,9 @@
 #print("Sample ETL automation framework")
+from idlelib import query
+
+import numpy as np
 import pandas as pd
+from pandasql import sqldf
 from pip._internal.models import target_python
 
 
@@ -69,13 +73,68 @@ def check_duplicates(target_df, pkey):
         print(duplicate_df)
     return duplicate_count, duplicate_df
 
+#NUll check function
+def check_null(target_df, null_column):
+    null_rows = target_df[target_df[null_column].isnull()]
+    if null_rows.shape[0] > 0:
+        print("Null rows present in Target")
+    else:
+        print("NO Null rows present in Target")
+
+#Data compare
+#this function is only applicable for 2 identical dataframes, then only .compare() will work
+def data_compare(source, target):
+    failed = source.compare(target)
+    if failed.shape[0] > 0:
+        print("Data is not present in Target")
+        print("Failed...!!!")
+    else:
+        print("Data is matching...!!!")
+
+#alternate solution for Data compare,
+#using SQLdf
+def data_compare_sql(source, target):
+    query = """select * from source except select * from target
+                union all
+                select * from target except select * from source"""
+    failed = sqldf(query)
+    if failed.shape[0] > 0:
+        print("Data is not present in Target")
+        print("Failed...!!!")
+    else:
+        print("Data is matching...!!!")
+
+#function to identify the out of range, as Data quality check not Data validation check
+# we can use this kind of functon to identify the range like age>=18 and age<=100, Gender identification like Male and Female only,
+# to find date range as well
+def check_out_of_range(target_df, column, min_val, max_val):
+    query = f"""
+    select * from target_df where {column} < {min_val}
+    OR {column} > {max_val}"""
+
+    invalid_df = sqldf(query)
+    if invalid_df.shape[0] > 0:
+        print("Invalid records present in Target")
+        print("Failed...!!!")
+        print(invalid_df)
+    else:
+        print("All records are valid...!!!")
+
+
+
+
 #Call the functions from here
 source = read_file(r"C:\Users\Admin\PycharmProjects\etlAutomationTestingUsingPython\Input_Files\customers_records_S.csv", "csv")
-#target = read_file(r"C:\Users\Admin\PycharmProjects\etlAutomationTestingUsingPython\Input_Files\customers_records_T.csv", file_type="csv")
+#target = read_file(r"C:\Users\Admin\PycharmProjects\etlAutomationTestingUsingPython\Input_Files\customers_records_S.csv", file_type="csv")
 target_duplicate_df = read_file(r"C:\Users\Admin\PycharmProjects\etlAutomationTestingUsingPython\Input_Files\customers_records_T_with_DuplicateRec.csv", file_type="csv")
+target_range = read_file(r"C:\Users\Admin\PycharmProjects\etlAutomationTestingUsingPython\Input_Files\customers_records_S.csv", file_type="csv")
 
 #check_count(source,target)
 #check_column_count(source, target)
-check_duplicates(target_duplicate_df, 'customer_id')
+#check_duplicates(target_duplicate_df, 'customer_id')
+#check_null(target_duplicate_df, 'customer_id')
+#data_compare(source, target) #used same source file as target file, for exact comaprision
+#data_compare_sql(source, target) # using SQLdf
+check_out_of_range(target_range, 'age', 21, 59)
 # source_count = len(source)
 # print(source_count)
